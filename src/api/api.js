1fd892bfb7d6d42db925e9b4a9e722d8eee19700
kirtan Limbachiya <108492403+kirtan-226@users.noreply.github.com>
2025-08-04 15:19:09 +0200

    adding live api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
 // export const BACKEND_ENDPOINT = "https://demo.bharuchbaps.in/index.php/";
-export const BACKEND_ENDPOINT = "https://054zz2zyxj.execute-api.ap-south-1.amazonaws.com/prod/annkut";
+export const BACKEND_ENDPOINT = "https://054zz2zyxj.execute-api.ap-south-1.amazonaws.com/prod/annkut/";
