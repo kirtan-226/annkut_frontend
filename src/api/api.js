9b4e006cd2e2ecbfe737adac9e5395bd16e7bc7d
kirtan Limbachiya <108492403+kirtan-226@users.noreply.github.com>
2025-08-04 14:53:28 +0200

    adding live api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
 // export const BACKEND_ENDPOINT = "https://demo.bharuchbaps.in/index.php/";
-export const BACKEND_ENDPOINT = "http://annkut.local/index.php/";
+export const BACKEND_ENDPOINT = "http://annkut.42web.io/annkut-backend/";
